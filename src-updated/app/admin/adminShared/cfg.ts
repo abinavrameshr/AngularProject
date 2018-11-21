cd83152d7e4d068521029c28857f15aa44ead602
abinavrameshr <43980144+abinavrameshr@users.noreply.github.com>
2018-11-21 12:11:21 +0530

    Add files via upload

---
+++ src-updated/app/admin/adminShared/cfg.ts
@@ -0,0 +1,10 @@
+export class cfg {
+    constructor(
+        public title: string,
+        public resource: string,
+        public content: string,
+        public userName?: string,
+        public id?: string,
+        public status?: string
+    ){}
+}
